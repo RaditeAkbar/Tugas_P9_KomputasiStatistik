eca780f82b97142df055272524672b67a5f94c60
Radite <radite.akbar06@gmail.com>
2026-05-20 18:55:44 +0700

    Tambah Boxplot

--- Percobaan Pertemuan 9.R
+++ Percobaan Pertemuan 9.R
@@ -2,3 +2,14 @@ summary(mtcars)
 mean(mtcars$mpg)
 sd(mtcars$mpg)
 max(mtcars$mpg)
+
+# Histogram
+hist(mtcars$mpg,
+     main = "Histogram of Miles per Gallon",
+     col = 'red')
+
+# Try add in branch-baru
+barplot(mtcars$mpg,
+        main = "Barplot of Miles per Gallon",
+        col = "skyblue")
+
